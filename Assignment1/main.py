511bb0da557cbfd38261ebef96c6ba7f9e516574
selmut <selma.moqvist@gmail.com>
2023-03-30 16:48:14 +0200

    data

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,8 +1,10 @@
+import os
 import pandas as pd
 import numpy as np
 import pycaret as pyc
 
-df = pd.read_csv(r"./data/db_game.csv")
+data_path = os.path.join(os.path.dirname(__file__), 'data/db_game.csv')
+df = pd.read_csv(data_path)
 
 print(df)
 
