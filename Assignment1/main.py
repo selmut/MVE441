16dec48c2720344180347d8f9814b81259195c5c
selmut <selma.moqvist@gmail.com>
2023-03-31 11:57:06 +0200

    data fix 2.0

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,10 +1,26 @@
 import os
 import pandas as pd
+from sklearn.decomposition import PCA
 import numpy as np
 import pycaret as pyc
 
-data_path = os.path.join(os.path.dirname(__file__), 'data/db_game.csv')
+n = 2
+
+'''chess_path = os.path.join(os.path.dirname(__file__), 'data/chess_games.csv')
+df = pd.read_csv(chess_path)'''
+
+data_path = os.path.join(os.path.dirname(__file__), 'data/data.csv')
 df = pd.read_csv(data_path)
 
-print(df)
+'''twitter_path = os.path.join(os.path.dirname(__file__), 'data/twitter_MBTI.csv')
+df = pd.read_csv(twitter_path)'''
 
+#df['game_id'] = df['game_id']-1
+
+data_pca = PCA(n_components=n, svd_solver='full')
+# data_pca_feats = data_pca.fit_transform()
+
+#print(df['opening_response'].value_counts())
+#print(df['opening_response']=='declined')
+print(df.keys())
+print(df.head())
