faba6acce165405f5f63a31746c95723ccd488ae
Jendlet <t.jendle@hotmail.com>
2023-04-04 14:14:13 +0200

    gmm

--- Assignment1/Classifiers/gmm.py
+++ Assignment1/Classifiers/gmm.py
@@ -1,3 +1,15 @@
+from sklearn.mixture import GaussianMixture
+import numpy as np
+import pandas as pd
+
 class GMM:
-    def __init__(self):
-        pass
+    def __init__(self, num_classes):
+        self.pca_data = None
+        self.num_classes = num_classes
+
+    def read_pca(self, path):
+        self.pca_data = pd.read_csv(path).to_numpy()
+
+    def classify(self):
+        gmm_classification = GaussianMixture(n_components = self.num_classes).fit_predict(self.pca_data)
+        return gmm_classification
