1d0c781e017051e370cb6031afe6b4944f271451
selmut <selma.moqvist@gmail.com>
2023-04-04 14:23:37 +0200

    classifiers + pca data

--- Assignment1/Classifiers/gmm.py
+++ Assignment1/Classifiers/gmm.py
@@ -2,6 +2,7 @@ from sklearn.mixture import GaussianMixture
 import numpy as np
 import pandas as pd
 
+
 class GMM:
     def __init__(self, num_classes):
         self.pca_data = None
@@ -11,5 +12,5 @@ class GMM:
         self.pca_data = pd.read_csv(path).to_numpy()
 
     def classify(self):
-        gmm_classification = GaussianMixture(n_components = self.num_classes).fit_predict(self.pca_data)
+        gmm_classification = GaussianMixture(n_components=self.num_classes).fit_predict(self.pca_data)
         return gmm_classification
--- Assignment1/Classifiers/kmeans.py
+++ Assignment1/Classifiers/kmeans.py
@@ -1,3 +1,15 @@
-class KMeans:
-    def __init__(self):
+import pandas as pd
+from sklearn.cluster import KMeans
+
+
+class kMeans:
+    def __init__(self, num_clusters):
+        self.pca_data = None
+        self.num_clusters = num_clusters
+
+    def read_pca(self, path):
+        self.pca_data = pd.read_csv(path).to_numpy()
+
+    def classify(self):
+        kmeans_classification = KMeans(n_clusters=self.num_clusters).fit_predict(self.pca_data)
         pass
