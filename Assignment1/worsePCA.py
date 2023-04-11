955f27f013847a8fed5ab7e2e959a2f4260145ba
natfrie <natfrie@student.chalmers.se>
2023-04-12 00:35:12 +0200

    fix fix

--- Assignment1/worsePCA.py
+++ Assignment1/worsePCA.py
@@ -2,33 +2,32 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+from matplotlib import colors
 
 
 num_points = 1000
 data=np.zeros((num_points,3))
-pca_feats = list(range(1,3))
 
 labels=np.random.randint(0,2,num_points)
-xs=labels*6+np.random.normal(0,1,num_points)
-ys=np.random.normal(0,10,num_points)
 
-data[:,0]=xs
-data[:,1]=ys
+xs=np.random.uniform(0,10,num_points)
+ys=2*xs+labels*15
+xnoise=np.random.normal(0,1,num_points)
+ynoise=np.random.normal(0,1,num_points)
 
-
-
-def reduce_dim(data, n):
-    std_data = StandardScaler().fit_transform(data)
-    pca = PCA(n_components=n)
-    return pca.fit_transform(std_data)
+data[:,0]=xs+xnoise
+data[:,1]=ys+ynoise
 
 
-pca_data = reduce_dim(data, 1)
+std_data = StandardScaler().fit_transform(data)
+pca = PCA(n_components=1)
+pca_data=pca.fit_transform(std_data)
+r=pca.components_.flatten()
 
 
-plt.scatter(data[:,0],data[:,1], c=labels, s=5)
+c = ['tab:blue', 'tab:orange']
+plt.scatter(data[:,0],data[:,1], c=labels, s=5, cmap=colors.ListedColormap(c))
 
 plt.show()
-
-plt.scatter(np.zeros(num_points), pca_data.flatten(), c=labels, s=5)
+plt.scatter(pca_data*r[0],pca_data*r[1] , c=labels, s=5, cmap=colors.ListedColormap(c))
 plt.show()
