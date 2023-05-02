6c095448f5b6ef79c19f9fca0fea01de25894e63
selmut <selma.moqvist@gmail.com>
2023-05-02 14:22:31 +0200

    load data

---
+++ Assignment3/run_kmeans.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('data/Cancerdata.txt', sep="\t")
+new_keys = {'lab': 'label'}
+new_keys.update(dict(zip([f'Unnamed: {i}' for i in range(3000)], [f'Gene {i}' for i in range(3000)])))
+
+df = df.rename(columns=new_keys)
+print(df.head())
+
+
