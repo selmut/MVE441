b2c641eae881823d2f77ddfd7bc78c1c13a5e329
selmut <selma.moqvist@gmail.com>
2023-05-02 14:22:08 +0200

    load data

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -2,9 +2,8 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv('data/Cancerdata.txt', sep="\t")
-new_keys = {'lab': 'lab'}
+new_keys = {'lab': 'label'}
 new_keys.update(dict(zip([f'Unnamed: {i}' for i in range(3000)], [f'Gene {i}' for i in range(3000)])))
 
 df = df.rename(columns=new_keys)
-print(df.keys())
-
+print(df.head())
