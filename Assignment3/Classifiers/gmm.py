5ecfc347c180bc933d600e8e67ef519ca003ca49
selmut <selma.moqvist@gmail.com>
2023-05-02 13:29:05 +0200

    initial

---
+++ Assignment3/Classifiers/gmm.py
@@ -0,0 +1,14 @@
+from sklearn.mixture import GaussianMixture
+import numpy as np
+import pandas as pd
+
+
+class GMM:
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+
+    def fit_data(self, train_data, train_labels):
+        return GaussianMixture(n_components=self.num_classes).fit(train_data)
+
+    def predict(self, test_data, test_labels, model):
+        return model.predict(test_data)
