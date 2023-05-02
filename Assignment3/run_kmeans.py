6afe37924e4634b89d28315cf91ca1f8b9b44b5e
selmut <selma.moqvist@gmail.com>
2023-05-02 14:23:58 +0200

    load data

--- Assignment3/run_kmeans.py
+++ Assignment3/run_kmeans.py
@@ -6,6 +6,11 @@ new_keys = {'lab': 'label'}
 new_keys.update(dict(zip([f'Unnamed: {i}' for i in range(3000)], [f'Gene {i}' for i in range(3000)])))
 
 df = df.rename(columns=new_keys)
-print(df.head())
+
+data = df.loc[:, 'Gene 1':'Gene 2999']
+labels = df.loc[:, 'label']
+
+print(data.head())
+print(labels.head())
 
 
