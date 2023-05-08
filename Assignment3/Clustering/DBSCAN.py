49d14e40a05f50f604591b2a4773bb326c421261
natfrie <natfrie@student.chalmers.se>
2023-05-08 13:56:07 +0200

    epsilon

--- Assignment3/Clustering/DBSCAN.py
+++ Assignment3/Clustering/DBSCAN.py
@@ -1,10 +1,10 @@
 from sklearn.cluster import DBSCAN
 
 class DensityBasedClustering:
-    def __init__(self, n_min):
+    def __init__(self, max_dist,n_min):
+        self.max_dist=max_dist
         self.n_min = n_min
 
     def fit_data(self, train_data):
-        return DBSCAN(min_samples=self.n_min).fit(train_data)
-
+        return DBSCAN(eps= self.max_dist,min_samples=self.n_min).fit_predict(train_data)
 
